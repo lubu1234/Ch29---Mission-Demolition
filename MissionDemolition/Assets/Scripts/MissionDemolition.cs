2c7d11dfe4b7ff82946293cfef9e8864e4502525
unknown <JKONG17@CILABS-119B-21.scilsnet.rutgers.edu>
2015-10-01 21:58:12 -0400

    Justin K. - Mission Demolition

---
+++ MissionDemolition/Assets/Scripts/MissionDemolition.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum GameMode{
+	idle,
+	playing,
+	levelEnd
+
+}
+
+
+public class MissionDemolition : MonoBehaviour {
+
+	static public MissionDemolition S;
+
+	//Fields set in the Unity Inspector pane
+	public GameObject[] castles;
+	public GUIText gtLevel;
+	public GUIText gtScore;
+	public Vector3 castlePos;
+
+	public bool ___________;
+
+	//fields set dynamically
+	public int level; //current level
+	public int levelMax; //number of levels
+	public int shotsTaken;
+	public GameObject castle; //current castle
+	public GameMode mode = GameMode.idle; 
+	public string showing = "Slingshot"; //follow cam mode
+
+
+	// Use this for initialization
+	void Start () {
+
+		S = this;
+
+		level = 0;
+		levelMax = castles.Length;
+		StartLevel ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		ShowGT ();
+
+		//Check for level end
+		if (mode == GameMode.playing && Goal.goalMet) {
+			//change mode to stop checking for level end
+			mode = GameMode.levelEnd;
+
+			//zoom out
+			SwitchView("Both");
+
+			//start the next level in 2secs
+			Invoke("NextLevel",2f);
+			
+		}
+	
+	}
+
+
+	void StartLevel(){
+		//Get rid of the old castle if one exists
+		if (castle != null) {
+			Destroy (castle);
+		}
+
+		//Destroy old projectiles if they exist
+		GameObject[] gos = GameObject.FindGameObjectsWithTag ("Projectile");
+		foreach (GameObject pTemp in gos) {
+			Destroy (pTemp);
+		}
+
+		//Instantiate the new castle
+		castle = Instantiate (castles [level])as GameObject;
+		castle.transform.position = castlePos;
+		shotsTaken = 0;
+
+		//reset camera
+		SwitchView ("Both");
+		ProjectileLine.S.Clear ();
+
+		//reset goal
+		Goal.goalMet = false;
+
+		ShowGT ();
+
+		mode = GameMode.playing;
+	}
+
+
+	void NextLevel(){
+		level++;
+		if (level == levelMax) {
+			level = 0;
+		}
+		StartLevel ();
+	}
+
+
+
+		void ShowGT(){
+			//show the data in the gui texts
+			gtLevel.text = "Level: " + (level+1)+" of " +levelMax;
+			gtScore.text = "Shots Taken: " +shotsTaken;
+
+		}
+
+	void OnGUI(){
+		//draw the gui button for view switching at the top of the screen
+		Rect buttonRect = new Rect((Screen.width/2)-50, 10, 100 ,24);
+
+		switch(showing){
+		case "Slingshot":
+			if(GUI.Button(buttonRect, "Show Castle")){
+				SwitchView("Castle");
+			}
+
+			break;
+
+
+
+		case "Castle":
+			if(GUI.Button(buttonRect, "Show Slingshot")){
+				SwitchView("Slingshot");
+			}
+
+			break;
+
+		case "Both":
+			if(GUI.Button(buttonRect, "Show Slingshot")){
+				SwitchView("Slingshot");
+			}
+			break;
+
+		}
+	}
+
+
+	//static method that allows code anywhere to request a view change
+	static public void SwitchView(string eView){
+		S.showing = eView;
+		switch (S.showing) {
+		case "Slingshot":
+			FollowCam.S.poi = null;
+			break;
+
+
+		case "Castle":
+			FollowCam.S.poi = S.castle;
+			break;
+
+
+		case "Both":
+			FollowCam.S.poi = GameObject.Find("ViewBoth");
+			break;
+
+		}
+	}
+
+
+	//static method that allows code anywhere to increment shotsTaken
+	public static void ShotFired(){
+		S.shotsTaken++;
+	}
+
+
+
+
+	
+
+}
---
+++ MissionDemolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+	
+	public GameObject launchPoint;
+	static public Slingshot S;
+
+	//fields set in the Unity Inspector pane
+	public GameObject prefabProjectile;
+	public float velocityMult = 4f;
+	public bool _________;
+
+	//fields set dynamically
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+
+
+
+
+	void Awake(){
+		S = this;
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive (false);
+		launchPos = launchPointTrans.position;
+
+	}
+
+	void OnMouseEnter(){
+		//print ("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive (true);
+	}
+
+	void OnMouseExit(){
+		//print ("Slingshot:OnMouseExit()");
+		launchPoint.SetActive (false);
+	}
+
+	void OnMouseDown(){
+		//The player has pressed the button while over Slingshot
+		aimingMode = true;
+
+		//Instantiate a projectile
+		projectile = Instantiate (prefabProjectile) as GameObject;
+
+		//Start it at the LaunchPoint
+		projectile.transform.position = launchPos;
+
+		//Set it to is Kinematic now
+		projectile.rigidbody.isKinematic = true;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//If the Slingshot is not in the aimingMode don't run this
+		if (!aimingMode)
+			return;
+
+		//Get the current mouse position in the 2D screen coordinates
+		Vector3 mousePos2D = Input.mousePosition;
+
+		//Convert the mouse position to 3D world coordinates
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		//Find the delta from the launchPos to the mousePos3D
+		Vector3 mouseDelta = mousePos3D - launchPos;
+
+		//Limit mouseDelta to the radius of the Slignshot sphereCollider
+		float maxMagnitude = this.GetComponent<SphereCollider> ().radius;
+		if (mouseDelta.magnitude > maxMagnitude) {
+			mouseDelta.Normalize();
+			mouseDelta *= maxMagnitude;
+		}
+
+		//Move the projectile to this new position
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+
+		if (Input.GetMouseButtonUp (0)) {
+			//The mouse has been released
+			aimingMode = false;
+			projectile.rigidbody.isKinematic = false;
+			projectile.rigidbody.velocity = -mouseDelta * velocityMult;
+			FollowCam.S.poi = projectile;
+			projectile = null;
+
+			MissionDemolition.ShotFired();
+
+		}
+
+
+
+
+	
+	}
+}
